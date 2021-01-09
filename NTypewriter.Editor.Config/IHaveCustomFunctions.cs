c3e96c776eaf3f47c3e575849e876ef4166a1796
NeVeSpl <nevespl@gmail.com>
2021-01-09 18:49:09 +0100

    add NTypewriter.Editor.Config

---
+++ NTypewriter.Editor.Config/IHaveCustomFunctions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTypewriter.Editor
+{
+    public interface IHaveCustomFunctions
+    {
+        IEnumerable<Type> GetTypesThatContainsStaticFunctionsToImport();        
+    }
+}
---
+++ NTypewriter.Editor.Config/NTEditorFileAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NTypewriter.Editor
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public class NTEditorFileAttribute : Attribute
+    {
+    }
+}
