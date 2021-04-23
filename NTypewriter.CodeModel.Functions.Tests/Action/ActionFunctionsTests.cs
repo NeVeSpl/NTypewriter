54ba191e96baec94fab7b1e730d33ef8e1cc50d5
Greg Veres <greg@veresfamily.me>
2021-04-23 18:38:55 -0400

    Added unit tests for ReturnType for .Net Core
    Added the different common ways of specifying the return types in .Net Core api controllers:
    
    - just return the type
    - return the type wrapped in an ActionResult
    - just return the type - async
    - return the type wrapped in an ActionResult - async
    
    There is one more way that is possible, but probably not likely. The ReturnType code handles it, but we can't seem to unit test it. This is shown in the ActionWithEnumParam action in the controller. If you uncomment the ProducesResponseType attribute, the return type should be type: WeatherForecast. However, the code in the unit tests does not pick up the attribute argument. I don't know why. It should and it does in a real environment.
    
    For now the unit test is testing that IActionResult is returned, but that shouldn't really be the case.

--- NTypewriter.CodeModel.Functions.Tests/Action/ActionFunctionsTests.cs
+++ NTypewriter.CodeModel.Functions.Tests/Action/ActionFunctionsTests.cs
@@ -40,6 +40,7 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
 
             var expected = RemoveWhitespace(
                            @"[GetData:get]
+                             [GetDataNoBody: get]
                              [SomeAsync:put]
                              [SomeAsync2:delete]
                              [ActionWithEnumParam:post]");
@@ -64,12 +65,12 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
 
             var expected = RemoveWhitespace(
                               @"[GetData:intbody]
+                                [GetDataNoBody:]
                                 [SomeAsync:InputDTObody]
                                 [SomeAsync2:InputDTObody]
                                 [ActionWithEnumParam:]");
             Assert.AreEqual(expected, actual);
         }
-
 
         [TestMethod]
         public async Task Url()
@@ -88,6 +89,7 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
 
             var expected = RemoveWhitespace(
                            @"[GetData:WeatherForecast/hkk]
+                             [GetDataNoBody:WeatherForecast/hkk/${url}]
                              [SomeAsync:sd?page=${pagg.page}&limit=${pagg.limit}]
                              [SomeAsync2:WeatherForecast/akacja/${par1}/${par2}/${par3}?par4=${par4}&par5=${par5}]
                              [ActionWithEnumParam:WeatherForecast?numbers=${numbers}&optional=${optional}&date=${date}]");
@@ -95,6 +97,52 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod]
+        public async Task ReturnType()
+        {
+            var template = @"{{- capture result
+                                 for class in data.Classes | Types.ThatInheritFrom ""ControllerBase"" 
+                                     for method in class.Methods }}                       
+                                      [{{- method.Name }} : {{- method | Action.ReturnType }}]
+                                   {{- end
+                                 end 
+                                 end 
+                                 Save result ""Some name"" }}
+                            ";
+            var result = await NTypeWriter.Render(template, data, settings);
+            var actual = RemoveWhitespace(result.Items.First().Content);
 
+            var expected = RemoveWhitespace(
+                            @"[GetData:IEnumerable<WeatherForecast>]
+                              [GetDataNoBody:IEnumerable<WeatherForecast>]
+                              [SomeAsync:IEnumerable<WeatherForecast>]
+                              [SomeAsync2:IEnumerable<WeatherForecast>]
+                              [ActionWithEnumParam:IActionResult]");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task ReturnType_with_uwrap()
+        {
+            var template = @"{{- capture result
+                                 for class in data.Classes | Types.ThatInheritFrom ""ControllerBase"" 
+                                     for method in class.Methods }}                       
+                                      [{{- method.Name }} : {{- method | Action.ReturnType | Type.Unwrap }}]
+                                   {{- end
+                                 end 
+                                 end 
+                                 Save result ""Some name"" }}
+                            ";
+            var result = await NTypeWriter.Render(template, data, settings);
+            var actual = RemoveWhitespace(result.Items.First().Content);
+
+            var expected = RemoveWhitespace(
+                            @"[GetData:WeatherForecast]
+                              [GetDataNoBody:WeatherForecast]
+                              [SomeAsync:WeatherForecast]
+                              [SomeAsync2:WeatherForecast]
+                              [ActionWithEnumParam:IActionResult]");
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
