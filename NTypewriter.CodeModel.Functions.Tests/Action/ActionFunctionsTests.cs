d8762ecbdf6371448bbd7415b7a718091636807a
Greg Veres <greg@veresfamily.me>
2021-04-23 15:20:54 -0400

    added withoutBodyParameter argument to Parameters
    the withoutBodyParameter flag to Parameters allows the user to return the list of parameters for the method excluding the parameter that will be sent in the body of the request.
    
    Unit tests were added, including a unit test for without this flag since that did not already exist.

--- NTypewriter.CodeModel.Functions.Tests/Action/ActionFunctionsTests.cs
+++ NTypewriter.CodeModel.Functions.Tests/Action/ActionFunctionsTests.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NTypewriter.CodeModel.Roslyn;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NTypewriter.CodeModel.Functions.Tests.Method
 {
@@ -38,7 +35,7 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
             var result = await NTypeWriter.Render(template, data, settings);
             var actual = RemoveWhitespace(result.Items.First().Content);
 
-            var expected = "[GetData:get][SomeAsync:put][SomeAsync2:delete]";
+            var expected = "[GetData:get][GetDataNoBody:get][SomeAsync:put][SomeAsync2:delete]";
             Assert.AreEqual(expected, actual);
         }
 
@@ -58,7 +55,65 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
             var result = await NTypeWriter.Render(template, data, settings);
             var actual = RemoveWhitespace(result.Items.First().Content);
 
-            var expected = "[GetData:intbody][SomeAsync:InputDTObody][SomeAsync2:InputDTObody]";
+            var expected = "[GetData:intbody][GetDataNoBody:][SomeAsync:InputDTObody][SomeAsync2:InputDTObody]";
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public async Task Parameters()
+        {
+            var template = @"{{- capture result }}
+                                {{- for class in data.Classes | Types.ThatInheritFrom ""ControllerBase"" }}
+                                   {{- for method in class.Methods }}                            
+                                      [{{- method.Name }} : {{- method | Action.Parameters }}]
+                                   {{- end }}
+                                {{- end }}
+                             {{- end }}
+                             {{- Save result ""Some name"" }}
+                            ";
+            var result = await NTypeWriter.Render(template, data, settings);
+            var actual = RemoveWhitespace(result.Items.First().Content);
+
+            var expected = "[GetData:[intbody]][GetDataNoBody:[inturl]][SomeAsync:[InputDTObody,Pagginationpagg]][SomeAsync2:[intpar3,InputDTObody,doublepar1,boolpar2,intpar4,intpar5]]";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task Parameters_without_body_parameters_true()
+        {
+            var template = @"{{- capture result }}
+                                {{- for class in data.Classes | Types.ThatInheritFrom ""ControllerBase"" }}
+                                   {{- for method in class.Methods }}                            
+                                      [{{- method.Name }} : {{- method | Action.Parameters true }}]
+                                   {{- end }}
+                                {{- end }}
+                             {{- end }}
+                             {{- Save result ""Some name"" }}
+                            ";
+            var result = await NTypeWriter.Render(template, data, settings);
+            var actual = RemoveWhitespace(result.Items.First().Content);
+
+            var expected = "[GetData:[]][GetDataNoBody:[inturl]][SomeAsync:[Pagginationpagg]][SomeAsync2:[intpar3,doublepar1,boolpar2,intpar4,intpar5]]";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public async Task Parameters_without_body_parameters_false()
+        {
+            var template = @"{{- capture result }}
+                                {{- for class in data.Classes | Types.ThatInheritFrom ""ControllerBase"" }}
+                                   {{- for method in class.Methods }}                            
+                                      [{{- method.Name }} : {{- method | Action.Parameters false }}]
+                                   {{- end }}
+                                {{- end }}
+                             {{- end }}
+                             {{- Save result ""Some name"" }}
+                            ";
+            var result = await NTypeWriter.Render(template, data, settings);
+            var actual = RemoveWhitespace(result.Items.First().Content);
+
+            var expected = "[GetData:[intbody]][GetDataNoBody:[inturl]][SomeAsync:[InputDTObody,Pagginationpagg]][SomeAsync2:[intpar3,InputDTObody,doublepar1,boolpar2,intpar4,intpar5]]";
             Assert.AreEqual(expected, actual);
         }
 
@@ -78,7 +133,7 @@ namespace NTypewriter.CodeModel.Functions.Tests.Method
             var result = await NTypeWriter.Render(template, data, settings);
             var actual = RemoveWhitespace(result.Items.First().Content);
 
-            var expected = "[GetData:WeatherForecast/hkk][SomeAsync:sd?page=${pagg.page}&limit=${pagg.limit}][SomeAsync2:WeatherForecast/akacja/${par1}/${par2}/${par3}?par4=${par4}&par5=${par5}]";
+            var expected = "[GetData:WeatherForecast/hkk][GetDataNoBody:WeatherForecast/hkk/${url}][SomeAsync:sd?page=${pagg.page}&limit=${pagg.limit}][SomeAsync2:WeatherForecast/akacja/${par1}/${par2}/${par3}?par4=${par4}&par5=${par5}]";
                           
             Assert.AreEqual(expected, actual);
         }
