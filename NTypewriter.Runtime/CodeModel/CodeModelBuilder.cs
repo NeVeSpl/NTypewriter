4b816091f3cd0117dd9e8ae3fbddfe72e9d2c72f
NeVeSpl <nevespl@gmail.com>
2021-11-17 17:26:30 +0100

    CodeModelBuilder is going opensource

---
+++ NTypewriter.Runtime/CodeModel/CodeModelBuilder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using NTypewriter.CodeModel;
+using NTypewriter.CodeModel.Roslyn;
+using NTypewriter.Editor.Config;
+using NTypewriter.Runtime.CodeModel.Internals;
+
+namespace NTypewriter.Runtime.CodeModel
+{
+    public class CodeModelBuilder
+    {
+        public async Task<ICodeModel> Build(Solution solution, IEditorConfig editorConfig)
+        {
+            var compositeCodeModel = new CompositeCodeModel();
+            var projectGraph = solution.GetProjectDependencyGraph();
+            var projectsToBeSearched = new HashSet<string>(editorConfig.ProjectsToBeSearched);
+
+            foreach (ProjectId projectId in projectGraph.GetTopologicallySortedProjects())
+            {
+                Project project = solution.GetProject(projectId);
+                bool isProjectOnTheList = projectsToBeSearched.Contains(project.Name);
+
+                if ((project.SupportsCompilation) && (isProjectOnTheList))
+                {
+                    var codeModel = await CreateCodeModel(project, editorConfig.NamespacesToBeSearched, editorConfig.SearchInReferencedProjectsAndAssemblies);
+                    compositeCodeModel.Add(codeModel);
+                }
+            }
+
+            return compositeCodeModel;
+        }
+
+        private async Task<NTypewriter.CodeModel.Roslyn.CodeModel> CreateCodeModel(Project project, IEnumerable<string> namespacesToBeSearched, bool searchInReferencedProjectsAndAssemblies)
+        {
+            var codeModelConfig = new CodeModelConfiguration();
+            codeModelConfig.OmitSymbolsFromReferencedAssemblies = !searchInReferencedProjectsAndAssemblies;
+            if (namespacesToBeSearched != null && namespacesToBeSearched.Any())
+            {
+                codeModelConfig.FilterByNamespace(namespacesToBeSearched.ToArray());
+            }
+
+            var compilation = await project.GetCompilationAsync();
+            var diagnostics = compilation.GetDiagnostics();
+            foreach (var diagnostic in diagnostics)
+            {
+                if (diagnostic.Severity != DiagnosticSeverity.Hidden)
+                {
+                    // that is so nice place for breakpoint in moment of need
+                }
+            }
+
+            var codeModel = new NTypewriter.CodeModel.Roslyn.CodeModel(compilation, codeModelConfig);
+            return codeModel;
+        }
+    }
+}
---
+++ NTypewriter.Runtime/CodeModel/Internals/CompositeCodeModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NTypewriter.CodeModel;
+
+namespace NTypewriter.Runtime.CodeModel.Internals
+{
+    class CompositeCodeModel : ICodeModel
+    {
+        private readonly List<ICodeModel> codeModels = new List<ICodeModel>();
+
+        public IEnumerable<IClass> Classes
+        {
+            get
+            {
+                foreach (var codeModel in codeModels)
+                {
+                    foreach (var @class in codeModel.Classes)
+                    {
+                        yield return @class;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<IDelegate> Delegates
+        {
+            get
+            {
+                foreach (var codeModel in codeModels)
+                {
+                    foreach (var @delegate in codeModel.Delegates)
+                    {
+                        yield return @delegate;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<IEnum> Enums
+        {
+            get
+            {
+                foreach (var codeModel in codeModels)
+                {
+                    foreach (var @enum in codeModel.Enums)
+                    {
+                        yield return @enum;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<IInterface> Interfaces
+        {
+            get
+            {
+                foreach (var codeModel in codeModels)
+                {
+                    foreach (var @interface in codeModel.Interfaces)
+                    {
+                        yield return @interface;
+                    }
+                }
+            }
+        }
+
+
+        public void Add(ICodeModel codeModel)
+        {
+            codeModels.Add(codeModel);
+        }
+    }
+}
