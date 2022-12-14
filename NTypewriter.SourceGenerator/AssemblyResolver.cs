cde8ebfca4e032b85f43b244167fba27ce69aa96
NeVeSpl <nevespl@gmail.com>
2022-12-14 19:24:23 +0100

    add NTypewriter.SourceGenerator project

---
+++ NTypewriter.SourceGenerator/AssemblyResolver.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace NTypewriter.SourceGenerator
+{
+    internal static class AssemblyResolver
+    { 
+        public static Assembly Resolve(object sender, ResolveEventArgs args)
+        {
+            var asmName = new AssemblyName(args.Name);
+            Trace.WriteLine($"AssemblyResolver.Resolve : {asmName.FullName}");
+
+            var loadedAssembly = AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(a => a.GetName().FullName == asmName.FullName);
+            if (loadedAssembly != null)
+            {
+                Trace.WriteLine($"AssemblyResolver.Resolve : resolved from AppDomain.CurrentDomain");
+                return loadedAssembly;
+            }
+
+            string resourceName = $"NTypewriter.SourceGenerator.{asmName.Name}.dll";
+
+            using (Stream resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null)
+                {
+                    Trace.WriteLine($"AssemblyResolver.Resolve : failure");
+                    return null;
+                }
+                using (MemoryStream memoryStream = new MemoryStream())
+                {
+                    Trace.WriteLine($"AssemblyResolver.Resolve : resolved from ResourceStream");
+                    resourceStream.CopyTo(memoryStream);
+                    return Assembly.Load(memoryStream.ToArray());
+                }
+            }
+        }
+    }
+}
---
+++ NTypewriter.SourceGenerator/SourceGenerator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using NTypewriter.CodeModel.Roslyn;
+
+namespace NTypewriter.SourceGenerator
+{
+    [Generator]
+    public class NTypewriterSourceGenerator : ISourceGenerator
+    {
+        private static long ExecuteCount = 0;
+        private static string Version = typeof(NTypewriterSourceGenerator).Assembly.GetName().Version.ToString();
+
+        static NTypewriterSourceGenerator()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolver.Resolve;
+        }
+
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            Interlocked.Increment(ref ExecuteCount);
+            context.ReportDiagnostic(Diagnostic.Create(Diagnostics.ExecuteInfo, Location.None, $"version : {Version}, total runs : {ExecuteCount}, last run : {DateTime.Now}"));           
+
+            try
+            {
+                ProofOfConcept(context.Compilation, context.AdditionalFiles);
+            }
+            catch (Exception ex)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(Diagnostics.Exception, Location.None, ex.ToString()));
+            }
+
+            //context.AddSource($"NTypewriterSourceGenerator.g.cs", sb.ToString());            
+        }
+
+        
+        private void ProofOfConcept(Compilation compilation, ImmutableArray<AdditionalText> additionalFiles)
+        {            
+            var codeModelConfiguration = new CodeModelConfiguration() { OmitSymbolsFromReferencedAssemblies = true };
+            var codeModel = new NTypewriter.CodeModel.Roslyn.CodeModel(compilation, codeModelConfiguration);
+
+            var template = additionalFiles.FirstOrDefault(x => x.Path.EndsWith(".nt"));
+            if (template == null) return;
+            
+            var ntypewriterConfig = new Configuration();
+            NTypeWriter.Render(template.GetText().ToString(), codeModel, ntypewriterConfig).ContinueWith(x => ParseResult(x.Result, template.Path));
+
+        }
+
+        private void ParseResult(Result result, string templateFilePath)
+        {
+            if (!result.HasErrors)
+            {
+                var renderedItem = result.Items.First();
+                var rootDirectory = Path.GetDirectoryName(templateFilePath);
+                var path = Path.Combine(rootDirectory, renderedItem.Name);
+                File.WriteAllText(path, renderedItem.Content);
+            }
+            else
+            {
+                foreach (var msg in result.Messages)
+                {
+                    Trace.WriteLine(msg.Message);
+                }
+            }
+        }
+    }
+}
