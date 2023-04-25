c325b90abc675da0a936c9686ea9d86a4d064f8b
amdavie <amdavie@gmail.com>
2023-04-25 16:31:23 -0600

    Only add rendered templates to compilation if they are being rendered to the project directory

--- NTypewriter.SourceGenerator/Adapters/GeneratedFileReaderWriter.cs
+++ NTypewriter.SourceGenerator/Adapters/GeneratedFileReaderWriter.cs
@@ -9,25 +9,27 @@ namespace NTypewriter.SourceGenerator.Adapters
     internal class GeneratedFileReaderWriter : IGeneratedFileReaderWriter
     {
         private readonly GeneratorExecutionContext generatorExecutionContext;
+        private readonly string projectDir;
 
-        public GeneratedFileReaderWriter(GeneratorExecutionContext generatorExecutionContext)
+        public GeneratedFileReaderWriter(GeneratorExecutionContext generatorExecutionContext, string projectDir)
         {
             this.generatorExecutionContext = generatorExecutionContext;
+            this.projectDir = projectDir;
         }
 
         public bool Exists(string path)
         {
-            return !IsCSharpFile(path) && File.Exists(path);
+            return !ShouldAddToCompilation(path) && File.Exists(path);
         }
 
         public Task<string> Read(string path)
         {
-            return Task.FromResult(IsCSharpFile(path) ? string.Empty : File.ReadAllText(path));
+            return Task.FromResult(ShouldAddToCompilation(path) ? string.Empty : File.ReadAllText(path));
         }
 
         public Task Write(string path, string text)
         {
-            if (IsCSharpFile(path))
+            if (ShouldAddToCompilation(path))
                 WriteCompilationSource(path, text);
             else
                 WriteFile(path, text);
@@ -36,6 +38,8 @@ namespace NTypewriter.SourceGenerator.Adapters
         }
 
         private static bool IsCSharpFile(string path) => Path.GetExtension(path).Equals(".cs", StringComparison.OrdinalIgnoreCase);
+        private bool IsSubPathOfProject(string path) => path.StartsWith(projectDir, StringComparison.OrdinalIgnoreCase);
+        private bool ShouldAddToCompilation(string path) => IsCSharpFile(path) && IsSubPathOfProject(path);
 
         private void WriteCompilationSource(string path, string source) => generatorExecutionContext.AddSource(GenerateHintNameFromPath(path), source);
 
